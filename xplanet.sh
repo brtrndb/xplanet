31493e91e9b4624a358a4996633019471a215fb6
Bertrand B <boyer.bertrand@gmail.com>
2018-10-26 15:51:46 +0200

    Rewritten script.

--- xplanet.sh
+++ xplanet.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Update desktop wallpaper with xplanet.
-# Tested on Ubuntu 15.10.
+# Bertrand B.
 
 # xplanet options.
 PLANET=earth			# Default planet.
@@ -21,96 +20,109 @@ STARS_IMG=$DIR_IMG/stars.png
 NOTIFY_IMG=$DIR_IMG/notify.jpg
 
 # Download clouds map urls.
-URL_CLOUDS="http://home.megapass.co.kr/~gitto88/cloud_data/clouds_4096.jpg"
-#URL_CLOUDS=http://xplanetclouds.com/free/local/clouds_2048.jpg
+URL_CLOUDS=http://xplanetclouds.com/free/local/clouds_2048.jpg
+URL_PING=www.google.com
 
 NOTIFY_CMD=/usr/bin/notify-send
 NULL=/dev/null
 
-xplanet_usage()
+usage()
 {
-    echo "Usage: $0 [OPTION]";
-    echo "  -c, --config	Use an xplanet configuration file. Default name: $CONFIG_FILE.";
-    echo "  -p, --planet	Choose a specific planet to render. Default: $PLANET.";
-    echo "  -dl		Download the clouds map. Works only for Earth.";
-    echo "  -bg		Set the rendered image as background.";
-    echo "  -h, --help	Display usage.";
-    return 0;
+    echo "Usage: ./$(basename "$0") [OPTION]";
+    echo "-c, --config: Use an xplanet configuration file. Default name: $CONFIG_FILE.";
+    echo "-p, --planet: Choose a specific planet to render. Default: $PLANET.";
+    echo "-dl:          Download the clouds map. Works only for Earth.";
+    echo "-bg:          Set the rendered image as background.";
+    echo "-h, --help:   Display usage.";
 }
 
-while [ "$#" -gt "0" ];
-do
-    case "$1" in
-	-c | --config)	# Choose a configuration file.
-	    CONFIG_FILE="$2";
-	    shift 2;
-	    ;;
-	-p | --planet)	# Planet.
-	    PLANET="$2";
-	    PLANET_IMG=$DIR_IMG/bg-$PLANET-`date +%s`.png;
-	    shift 2;
-	    ;;
-	-dl)		# Download earth clouds map.
-	    DL_CLOUDS=true;
-	    shift 1;
-	    ;;
-	-bg | --background)	# Set the image as background.
-	    SET_BACKGROUND=true;
-	    shift 1;
-	    ;;
-	-h | --help)	# Display usage.
-	    xplanet_usage;
-	    return 0;
-	    ;;
-	-* | --*)	# Unknown option found
-	    xplanet_usage;
-	    return 1;
-	    ;;
-    esac
-done
+configure(){
+    while [ "$#" -gt "0" ]; do
+	case "$1" in
+	    -c | --config)	# Choose a configuration file.
+		CONFIG_FILE="$2";
+		shift 2;
+		;;
+	    -p | --planet)	# Planet.
+		PLANET="$2";
+		PLANET_IMG=$DIR_IMG/bg-$PLANET-`date +%s`.png;
+		shift 2;
+		;;
+	    -dl)		# Download earth clouds map.
+		DL_CLOUDS=true;
+		shift 1;
+		;;
+	    -bg | --background)	# Set the image as background.
+		SET_BACKGROUND=true;
+		shift 1;
+		;;
+	    -h | --help)	# Display usage.
+		usage;
+		exit 0;
+		;;
+	    -* | --*)		# Unknown option found
+		echo "Unknown parameter $1. Ignored.";
+		shift 1;
+		;;
+	esac
+    done
+}
 
-
-echo "Xplanet wallpaper script. (`date "+%H:%M:%S"`).";
-
-if [ "$PLANET" = "earth" ] && [ "$DL_CLOUDS" = "true" ];
-then
-    echo " > Planet Earth needs clouds map. ";
-    echo -n " > Checking internet connexion.";
-    if ping -c 3 google.com > $NULL 2>&1;
-    then
-	echo " Connected !";
-	echo -n " > Deleting old clouds map.";
-	rm -f $CLOUDS_IMG;
-	echo " Done !";
-	echo -n " > Downloading clouds map...";
-	wget $URL_CLOUDS -O $CLOUDS_IMG > $NULL 2>&1
-	echo " Done !";
-    else
-	echo " Connection failed.";
-	echo -n " > Old clouds map used instead.";
+dl_clouds(){
+    echo "Planet Earth requires clouds map.";
+    echo -n "Checking internet connexion...";
+    CONNECTED=`ping -c 1 $URL_PING && echo $?`;
+    if [ ! "0" -eq "$CONNECTED" ] ; then
+	echo " There is no internet connection.";
+	echo "Old clouds map will be used.";
+	return;
     fi
-else
-    echo " > Planet selected: $PLANET." ;
-fi
 
-echo -n " > Deleting old backround images.";
-rm -f $DIR_IMG/bg-*.png;
-echo " Done !";
+    echo " Success.";
+    echo -n "Deleting old clouds map.";
+    rm -f $CLOUDS_IMG;
+    echo " Done.";
+    echo "Downloading clouds map.";
+    wget -q $URL_CLOUDS -O $CLOUDS_IMG --show-progress --progress=bar:scroll 2>&1 ;
+}
 
-echo -n " > Generating new background image...";
-if [ "$LABEL" = "true" ];
-then
-    OPT_LABEL=-label -color $LABEL_COLOR -labelpos $LABEL_POS -fontsize 25
-fi
-xplanet -num_times 1 -config $CONFIG_FILE -body $PLANET -longitude $COORD_LONG -latitude $COORD_LAT -background $STARS_IMG -geometry $GEOMETRY -quality 100 $OPT_LABEL -output $PLANET_IMG > $NULL 2>&1;
-echo " Done !";
+delete_previous(){
+    echo -n "Deleting old backround images.";
+    rm -f $DIR_IMG/bg-*.png;
+    echo " Done.";
+}
 
-if [ "$SET_BACKGROUND" = "true" ];
-   then
-       echo -n " > Setting up background.";
-       gsettings set org.gnome.desktop.background picture-uri file://"$(readlink -f $PLANET_IMG)";
-fi
+render(){
+    echo -n "Generating image. This may take a while...";
+    if [ "$LABEL" = "true" ]; then
+	OPT_LABEL=-label -color $LABEL_COLOR -labelpos $LABEL_POS -fontsize 25
+    fi
+    xplanet -num_times 1 -config $CONFIG_FILE -body $PLANET -longitude $COORD_LONG -latitude $COORD_LAT -background $STARS_IMG -geometry $GEOMETRY -quality 100 $OPT_LABEL -output $PLANET_IMG > $NULL 2>&1;
+    echo " Done !";
+}
 
-$NOTIFY_CMD --icon=$NOTIFY_IMG --app-name=$0 "Xplanet: Mise à jour terminée.";
-echo " Done !";
-return 0;
+set_background(){
+    if [ "$SET_BACKGROUND" = "true" ]; then
+	echo -n "Setting up background...";
+	gsettings set org.gnome.desktop.background picture-uri file://"$(readlink -f $PLANET_IMG)";
+	echo " Done !";
+    fi
+}
+
+notify(){
+    $NOTIFY_CMD --icon=$NOTIFY_IMG --app-name=$0 "Xplanet: Explore the stars !";
+}
+
+run(){
+    configure $*;
+    echo "Planet selected: $PLANET." ;
+    if [ "$PLANET" = "earth" ] && [ "$DL_CLOUDS" = "true" ] ; then
+	dl_clouds;
+    fi
+    delete_previous;
+    render;
+    set_background;
+    notify;
+}
+
+run $*;
