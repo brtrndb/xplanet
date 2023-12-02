573f82597006d208d3d5f16b563c0235de0684fa
Bertrand B. <boyer.bertrand@gmail.com>
2023-12-02 21:06:19 +0100

    Rewrite and update script.

--- xplanet.sh
+++ xplanet.sh
@@ -1,84 +1,83 @@
 #!/bin/sh
 # Bertrand B.
 
+NULL="/dev/null";
+SCRIPT_PATH="$(cd -- "$(dirname "$0")" > $NULL 2>&1 ; pwd -P)";
+TEXTURES_FOLDER="$SCRIPT_PATH/img";
+
 # Script parameters.
-PARAM_CONFIG_FILE="$(dirname "$0")/xplanet.config";
-PARAM_TARGET_DIR="$(dirname "$0")/img";
-PARAM_PLANET=earth;
-PARAM_CRON=false;
-PARAM_CLOUDS=false;
-PARAM_BACKGROUND=false;
-PARAM_REMOVE=false;
-PARAM_LABEL=false;
+CONFIG_FILE="$SCRIPT_PATH/xplanet.config";
+PLANET=earth;
+SETUP_CRON=false;
+SETUP_BACKGROUND=false;
+DELETE_PREVIOUS=false;
+INCLUDE_LABEL=false;
+NOTIFY=true
 
-# Xplanet options.
-COORD_LONG=0;			# Default longitude.
-COORD_LAT=0;			# Default latitude.
-LABEL_COLOR=blue;		# Deafult label text color.
-LABEL_POS=+0+0;			# Default label position.
-GEOMETRY=4096x2048;		# Default image size.
-
-# Images.
-FOLDER_IMG="$(dirname "$0")/img";
-IMG_PLANET="$PARAM_TARGET_DIR/bg-$PARAM_PLANET-$(date +%s).png";
-IMG_CLOUDS="$FOLDER_IMG/earth-clouds.jpg";
-IMG_STARS="$FOLDER_IMG/stars.png";
-IMG_NOTIFY="$FOLDER_IMG/notify.jpg";
-
-# Download clouds map urls.
-URL_CLOUDS="https://raw.githubusercontent.com/apollo-ng/cloudmap/master/global.jpg";
-URL_PING="www.google.com";
+EARTH_CLOUDS_MAP_URL="https://matteason.github.io/daily-cloud-maps/8192x4096-clouds.jpg";
+PING_URL="www.google.com";
 
 NOTIFY_CMD="/usr/bin/notify-send";
-NULL="/dev/null";
+IMG_NOTIFY_ICON="$TEXTURES_FOLDER/notify.jpg";
+
+# Xplanet options.
+LONGITUDE=0;
+LATITUDE=0;
+LABEL_COLOR=blue;
+LABEL_POS=+0+0;
+GEOMETRY=4096x2048;
+BACKGROUND_STARS="$TEXTURES_FOLDER/stars.png";
 
 # Used for setting background.
 PID=$(pgrep gnome-session -n);
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-);
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ | cut -d= -f2-);
 
 usage() {
   echo "Usage: ./$(basename "$0") [OPTION]";
-  echo "-f, --file:        Use an xplanet configuration file. Default name: $PARAM_CONFIG_FILE.";
-  echo "-t, --target:      Set the directory for generated images. Default: $PARAM_TARGET_DIR.";
-  echo "-p, --planet:      Choose a specific planet to render. Default: $PARAM_PLANET.";
+  echo "-f, --file:        Use an xplanet configuration file. Default name: $CONFIG_FILE.";
+  echo "-p, --planet:      Choose a specific planet to render. Default: $PLANET.";
   echo "-c, --cron:        Add a crontab entry every hour.";
-  echo "-dl, --download:   Download the clouds map. Works only for Earth.";
   echo "-bg, --background: Set the rendered image as background.";
-  echo "-rm, --remove:     Delete previous images.";
+  echo "-d, --delete:      Delete previous images.";
+  echo "-nn, --no-notify:  Disable notification once render is terminated.";
   echo "-h, --help:        Display usage.";
+}
+
+run() {
+  configure $*;
+
+  if [ "$SETUP_CRON" = "true" ]; then
+    run_cron_setup;
+  else
+    run_xplanet;
+  fi
 }
 
 configure() {
   while [ "$#" -gt "0" ]; do
     case "$1" in
       -f | --file)
-        PARAM_CONFIG_FILE="$2";
-        shift 2;
-        ;;
-      -t | --target)
-        PARAM_TARGET_DIR="$2";
-        IMG_PLANET="$PARAM_TARGET_DIR/bg-$PARAM_PLANET-$(date +%s).png";
+        CONFIG_FILE="$2";
         shift 2;
         ;;
       -p | --planet)
-        PARAM_PLANET="$2";
-        IMG_PLANET="$PARAM_TARGET_DIR/bg-$PARAM_PLANET-$(date +%s).png";
+        PLANET="$2";
         shift 2;
         ;;
       -c | --cron)
-        PARAM_CRON=true;
-        shift 1;
-        ;;
-      -dl | --download)
-        PARAM_CLOUDS=true;
+        SETUP_CRON=true;
         shift 1;
         ;;
       -bg | --background)
-        PARAM_BACKGROUND=true;
+        SETUP_BACKGROUND=true;
         shift 1;
         ;;
-      -rm | --remove)
-        PARAM_REMOVE=true;
+      -d | --delete)
+        DELETE_PREVIOUS=true;
+        shift 1;
+        ;;
+      -nn | --no-notify)
+        NOTIFY=false;
         shift 1;
         ;;
       -h | --help)
@@ -86,83 +85,22 @@ configure() {
         exit 0;
         ;;
       --* | -*)
-        echo "Unknown parameter $1. Ignored.";
-        shift 1;
+        echo "Unknown parameter '$1'.";
+        usage;
+        exit 0;
         ;;
     esac
   done
 }
 
-mk_folder() {
-  if [ "$PARAM_TARGET_DIR" = "." ]; then
-    PARAM_TARGET_DIR=$(pwd);
-  fi
-
-  if [ ! -d "$PARAM_TARGET_DIR" ]; then
-    mkdir -vp "$PARAM_TARGET_DIR";
-  fi
-}
-
-dl_clouds() {
-  if [ "$PARAM_PLANET" != "earth" ] || [ "$PARAM_CLOUDS" = "false" ] ; then
-    return;
-  fi
-
-  echo -n "Clouds map required.";
-  CONNECTED=$(ping -c 1 $URL_PING > $NULL 2>&1 && echo $?);
-  if [ ! "0" -eq "$CONNECTED" ] ; then
-    echo " No internet connection. Old clouds map will be used.";
-    return;
-  fi
-
-  echo " Downloading clouds map.";
-  wget $URL_CLOUDS -O $IMG_CLOUDS -q --show-progress --progress=bar:scroll;
-}
-
-rm_previous_render() {
-  if [ "$PARAM_REMOVE" = "true" ]; then
-    LATEST_BACKGROUND=$(ls -t $PARAM_TARGET_DIR/bg-$PARAM_PLANET*.png | head -2 | tail -1)
-    rm -vf $LATEST_BACKGROUND;
-  fi
-}
-
-render() {
-  if [ "$PARAM_LABEL" = "true" ]; then
-    OPT_LABEL="-label -color $LABEL_COLOR -labelpos $LABEL_POS -fontsize 25";
-  fi
-  echo -n "Generating image. This may take a while...";
-  xplanet -num_times 1 \
-    -config $PARAM_CONFIG_FILE \
-    -searchdir $FOLDER_IMG \
-    -body $PARAM_PLANET \
-    -longitude $COORD_LONG \
-    -latitude $COORD_LAT \
-    -background $IMG_STARS \
-    $OPT_LABEL \
-    -geometry $GEOMETRY \
-    -quality 100 \
-    -output $IMG_PLANET > $NULL 2>&1;
-  echo " Done !";
-}
-
-set_background() {
-  if [ "$PARAM_BACKGROUND" = "true" ]; then
-    gsettings set org.gnome.desktop.background picture-uri "file://$(readlink -f "$IMG_PLANET")";
-  fi
-}
-
-notify() {
-  $NOTIFY_CMD --icon=$IMG_NOTIFY --app-name=$0 "Xplanet: Explore the stars !";
-}
-
-setup_cron() {
+run_cron_setup() {
   CRONTAB=$(crontab -u $USER -l 2> $NULL | grep -v $(basename "$0"));
-  CONF="0 * * * * $(readlink -f "$0") -f $(readlink -f "$PARAM_CONFIG_FILE") -bg -dl -rm -t $PARAM_TARGET_DIR";
+  CONF="0 * * * * $(readlink -f "$0") -f $(readlink -f "$CONFIG_FILE") -bg -d -nn";
 
   if [ -z "$CRONTAB" ]; then
-    echo -e "$CONF" | crontab -u $USER -;
+    echo -e "$CONF" | crontab -u "$USER" -;
   else
-    echo -e "$CRONTAB\n$CONF" | crontab -u $USER -;
+    echo -e "$CRONTAB\n$CONF" | crontab -u "$USER" -;
   fi
   if [ "$?" = "0" ]; then
     echo "Cron created: $CONF.";
@@ -171,22 +109,79 @@ setup_cron() {
   fi
 }
 
-do_xplanet() {
-  echo "Planet selected: $PARAM_PLANET." ;
-  mk_folder;
-  dl_clouds;
-  render;
-  rm_previous_render;
-  set_background;
+run_xplanet() {
+  RENDERED="$TEXTURES_FOLDER/bg-$PLANET-$(date +%s)-$GEOMETRY.png";
+
+  render "$RENDERED";
+  delete_previous_render;
+  set_background "$RENDERED";
   notify;
 }
 
-run() {
-  configure $*;
-  if [ "$PARAM_CRON" = "true" ]; then
-    setup_cron;
-  else
-    do_xplanet;
+render() {
+  echo "Rendering planet $PLANET. This may take a while...";
+
+  if [ "$PLANET" = "earth" ]; then
+    get_earth_clouds_map;
+  fi
+
+  if [ "$INCLUDE_LABEL" = "true" ]; then
+    OPT_LABEL="-label -color $LABEL_COLOR -labelpos $LABEL_POS -fontsize 25";
+  fi
+
+  xplanet -num_times 1 \
+    -config "$CONFIG_FILE" \
+    -searchdir "$TEXTURES_FOLDER" \
+    -body "$PLANET" \
+    -longitude $LONGITUDE \
+    -latitude $LATITUDE \
+    -background "$BACKGROUND_STARS" \
+    $OPT_LABEL \
+    -geometry $GEOMETRY \
+    -quality 100 \
+    -output "$1" > $NULL 2>&1;
+  echo "Done !";
+}
+
+get_earth_clouds_map() {
+  IS_CONNECTED=$(ping -c 1 $PING_URL > $NULL 2>&1 && echo $?);
+  if [ ! "0" -eq "$IS_CONNECTED" ] ; then
+    echo "No internet. Previous clouds map will be used if any.";
+    return;
+  fi
+
+  EARTH_CLOUDS_MAP="$TEXTURES_FOLDER/earth-clouds-$(date +%s).jpg";
+
+  echo "Downloading earth clouds map.";
+  wget $EARTH_CLOUDS_MAP_URL -O "$EARTH_CLOUDS_MAP" --no-cache -q -T 5;
+
+  LATEST_EARTH_CLOUDS_MAP=$(find "$TEXTURES_FOLDER" -type f -name earth-clouds-*.jpg | sort | tac | tail -n +2)
+  rm -f $LATEST_EARTH_CLOUDS_MAP;
+}
+
+delete_previous_render() {
+  if [ "$DELETE_PREVIOUS" = "true" ]; then
+    LATEST_BACKGROUND=$(find "$TEXTURES_FOLDER" -type f -name bg-"$PLANET"-*.png | sort | tac | tail -n +2)
+    rm -f $LATEST_BACKGROUND;
+  fi
+}
+
+set_background() {
+  if [ "$SETUP_BACKGROUND" = "true" ]; then
+    echo "Updating background.";
+
+    COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme);
+    if [ "$COLOR_SCHEME" = "'prefer-dark'" ]; then
+      gsettings set org.gnome.desktop.background picture-uri-dark "file://$(readlink -f "$1")";
+    else
+      gsettings set org.gnome.desktop.background picture-uri "file://$(readlink -f "$1")";
+    fi
+  fi
+}
+
+notify() {
+  if [ "$NOTIFY" = "true" ]; then
+    $NOTIFY_CMD --icon="$IMG_NOTIFY_ICON" --app-name="$0" "Xplanet: Explore the stars !";
   fi
 }
 
